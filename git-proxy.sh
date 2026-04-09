479bcf1b255c271cddaffa92dbe7cada0feaf6cf
Lucky1192405754 <1192405754@qq.com>
2026-04-10 00:51:45 +0800

    chore: 更新配置文件和资源文件

--- git-proxy.sh
+++ git-proxy.sh
@@ -3,3 +3,6 @@ git config --global http.proxy "http://127.0.0.1:7890"
 
 # 设置 https 代理
 git config --global https.proxy "http://127.0.0.1:7890"
+
+# 如果上面两根不行，使用Windows证书系统
+git config --global http.sslBackend schannel
